4624b2ed69c78ff877e9c8ee6cad824cb51b11d9
Framebuffer <26495556+Framebuffers@users.noreply.github.com>
2024-12-21 23:49:52 -0300

    wish me luck imma start back over

--- Scenarios/WorldLimits.cs
+++ Scenarios/WorldLimits.cs
@@ -6,6 +6,7 @@ namespace Intuition.Scenarios
 {
 	public partial class WorldLimits : Area3D
 	{
+		[Signal] public delegate void PlayerDeathEventHandler();
 		private void OnBodyExited(Node3D body)
 		{
 			$"Body out".ToConsole();
@@ -13,8 +14,19 @@ namespace Intuition.Scenarios
 			{
 				body.QueueFree();
 			}
+		}
 
-			// body.QueueFree();
+		private void WaterRisingRoutine(Node3D body)
+		{
+			$"Water rising\n Body to delete: {body.Name}".ToConsole();
+			if (body is not CharacterBody3D)
+			{
+				body.QueueFree();
+			}
+			else
+			{
+				EmitSignal(SignalName.PlayerDeath);
+			}
 		}
 	}
 }
